5a7e50c879c16dc0b8f2a578b295e819a32df336
Nb <42965940+622022@users.noreply.github.com>
2020-06-06 14:25:45 +0200

    working ofr /login end point

--- src/main/java/io/swagger/api/LoginApiController.java
+++ src/main/java/io/swagger/api/LoginApiController.java
@@ -1,20 +1,24 @@
 package io.swagger.api;
 
+import io.swagger.configuration.JwtTokenUtil;
 import io.swagger.model.Body1;
 import io.swagger.model.InlineResponse2002;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
+import io.swagger.model.JwtResponse;
+import io.swagger.service.JwtUserDetailsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.validation.constraints.*;
@@ -25,7 +29,18 @@ import java.util.List;
 import java.util.Map;
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2020-05-21T18:10:30.703Z[GMT]")
 @Controller
+@RestController
+@CrossOrigin
 public class LoginApiController implements LoginApi {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private JwtUserDetailsService userDetailsService;
 
     private static final Logger log = LoggerFactory.getLogger(LoginApiController.class);
 
@@ -40,18 +55,30 @@ public class LoginApiController implements LoginApi {
     }
 
     public ResponseEntity<InlineResponse2002> loginUser(@ApiParam(value = ""  )  @Valid @RequestBody Body1 body
-) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<InlineResponse2002>(objectMapper.readValue("{\n  \"tokenType\" : \"tokenType\",\n  \"userId\" : \"userId\",\n  \"tokenValue\" : \"tokenValue\"\n}", InlineResponse2002.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<InlineResponse2002>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
+)   {
+        //authenticate(authenticationRequest.getUsername(),
+        body.getPassword();
+        final UserDetails userDetails =
+                userDetailsService.loadUserByUsername(body.getUsername());
+        //JwtUserDetails userDetails = new JwtUserDetails();
+        //userDetails.setUsername(authenticationRequest.getUsername());
 
-        return new ResponseEntity<InlineResponse2002>(HttpStatus.NOT_IMPLEMENTED);
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+//        final String finalToken = new JwtResponse(token);
+        InlineResponse2002 response2002 = new InlineResponse2002("1","Bearer",token);
+        return new ResponseEntity<InlineResponse2002>(response2002,HttpStatus.OK);
+//        return ResponseEntity.ok(new JwtResponse(token));
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
     }
 
 }
