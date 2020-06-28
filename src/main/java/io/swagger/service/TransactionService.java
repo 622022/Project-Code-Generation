8e265111506797a48b25e89987569b6841956707
suicide <42965940+622022@users.noreply.github.com>
2020-06-28 13:08:36 +0200

    added more checks and a method to make transaction

--- src/main/java/io/swagger/service/TransactionService.java
+++ src/main/java/io/swagger/service/TransactionService.java
@@ -24,31 +24,29 @@ public class TransactionService {
 
 
     public Transaction createTransaction(Transaction transaction) {
+
+        //this ensures that only the accounts that are within the repository are used otherwise they will be null
         AccountObject accountSender = accountRepository.getAccountObjectByIBAN(transaction.getSender());
         AccountObject accountReceiver = accountRepository.getAccountObjectByIBAN(transaction.getReceiver());
 
         //checking if the accounts even exist & checking if they're not the same accounts
         if(accountSender != null && accountReceiver != null && accountSender != accountReceiver)
         {
-            // checking if accounts is of the same customer
-            if(accountSender.getOwnerId().equals(accountReceiver.getOwnerId()))
+            //checking if the account from where the money is being sent or to sent is a savings account
+            if(accountSender.getType() == AccountObject.TypeEnum.SAVING || accountReceiver.getType()== AccountObject.TypeEnum.SAVING)
             {
-                boolean successfulTransaction = accountSender.withdrawAmount(transaction.getAmount()); // withdraw from sender
-                accountReceiver.insertAmount(transaction.getAmount()); // add funds to receiver account
-
-                //check if transaction was successful
-                if(successfulTransaction == true)
+                // then checking if accounts is of the same customer
+                if(accountSender.getOwnerId().equals(accountReceiver.getOwnerId()))
                 {
-                    transactionRepository.save(transaction);
-                    accountService.editAccount(transaction.getSender(),accountSender);
-                    accountService.editAccount(transaction.getReceiver(),accountReceiver);
-
-                    System.out.println("==============test============");
-                    System.out.println(accountSender.toString());
-                    System.out.println(accountReceiver.toString());
-                    return transaction;
+                    return makeTransaction(accountSender,accountReceiver,transaction);
                 }
             }
+            else
+            {
+                return makeTransaction(accountSender,accountReceiver,transaction);
+            }
+
+
         }
         return null;
     }
@@ -63,6 +61,27 @@ public class TransactionService {
         });
 
         return transactionList;
+    }
+
+    public Transaction makeTransaction(AccountObject accountSender,AccountObject accountReceiver, Transaction transaction)
+    {
+        boolean successfulTransaction;
+        successfulTransaction = accountSender.withdrawAmount(transaction.getAmount()); // withdraw from sender
+        accountReceiver.insertAmount(transaction.getAmount()); // add funds to receiver account
+
+        //check if transaction was successful
+        if(successfulTransaction == true)
+        {
+            transactionRepository.save(transaction);
+            accountService.editAccount(transaction.getSender(),accountSender);
+            accountService.editAccount(transaction.getReceiver(),accountReceiver);
+
+            System.out.println("==============test============");
+            System.out.println(accountSender.toString());
+            System.out.println(accountReceiver.toString());
+            return transaction;
+        }
+        return null;
     }
 
 
