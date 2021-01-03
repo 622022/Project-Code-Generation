49215bcee9a15f668d19349c74e0ad2df5a33da1
HamzaG5 <42969091+HamzaG5@users.noreply.github.com>
2021-01-03 18:13:48 +0100

    updated condition for filtering transactions

--- src/main/java/io/swagger/dao/TransactionRepository.java
+++ src/main/java/io/swagger/dao/TransactionRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
-    @Query(nativeQuery = true, value = "SELECT * FROM Transaction WHERE sender LIKE :iBanSender OR receiver LIKE :iBanReceiver AND receiver_name LIKE %:receiverName% LIMIT :limit OFFSET :offset")
+    @Query(nativeQuery = true, value = "SELECT * FROM Transaction WHERE (sender LIKE :iBanSender OR receiver LIKE :iBanReceiver) AND (receiver_name LIKE %:receiverName%) LIMIT :limit OFFSET :offset")
     Iterable<Transaction> getAllTransactions(@Param("iBanSender") String iBanSender, @Param("iBanReceiver") String iBanReceiver, @Param("receiverName") String receiverName, @Param("limit") Integer limit, @Param("offset") int offset);
 
 }
